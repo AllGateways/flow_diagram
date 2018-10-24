5ede3fbd5247ab3ffd485b7b5ff95f2b82716298
Gang Zheng <gzheng@Gangs-MacBook-Pro.local>
2018-10-23 22:41:44 -0400

    format

--- appserver/static/visualizations/flow_diagram/src/flow_utils.js
+++ appserver/static/visualizations/flow_diagram/src/flow_utils.js
@@ -2,11 +2,11 @@ define([
     'jquery',
     'underscore',
     'jointjs'
-    ], function(
-        $,
-        _,
-        jointjs
-    ) {
+], function (
+    $,
+    _,
+    jointjs
+) {
         const BACKGROUND_COLOR = '#42b9f4';
 
         jointjs.dia.Link.define('flowchart.Link', {
@@ -19,14 +19,14 @@ define([
                     targetMarker: {
                         'type': 'path',
                         'd': 'M 7 -3.5 0 0 7 3.5 z',
-                         stroke: 'white',
-                         strokeWidth: 0.5,
-                         fill: 'black',
+                        stroke: 'white',
+                        strokeWidth: 0.5,
+                        fill: 'black',
                     },
-		    z: -1,
+                    z: -1,
                     connectionPoint: {
                         name: 'bbox',
-                        args: {stroke: true},
+                        args: { stroke: true },
                     }
                 },
                 wrapper: {
@@ -57,36 +57,36 @@ define([
         const PROCESS_D = '' +
             'M ' + (PROCESS_SIZE - PROCESS_RADIUS_SIZE) + ' 0 ' +
             'A ' + PROCESS_RADIUS_SIZE + ' ' + PROCESS_RADIUS_SIZE + ' 0 0 1 ' + PROCESS_SIZE + ' ' + PROCESS_RADIUS_SIZE + ' ' +
-            'L ' + PROCESS_SIZE + ' ' + (PROCESS_SIZE - PROCESS_RADIUS_SIZE) + ' ' + 
+            'L ' + PROCESS_SIZE + ' ' + (PROCESS_SIZE - PROCESS_RADIUS_SIZE) + ' ' +
             'A ' + PROCESS_RADIUS_SIZE + ' ' + PROCESS_RADIUS_SIZE + ' 0 0 1 ' + (PROCESS_SIZE - PROCESS_RADIUS_SIZE) + ' ' + PROCESS_SIZE + ' ' +
             'L ' + PROCESS_RADIUS_SIZE + ' ' + PROCESS_SIZE + ' ' +
-            'A ' + PROCESS_RADIUS_SIZE + ' ' + PROCESS_RADIUS_SIZE + ' 0 0 1 ' + 0 + ' ' + (PROCESS_SIZE - PROCESS_RADIUS_SIZE) + ' ' + 
+            'A ' + PROCESS_RADIUS_SIZE + ' ' + PROCESS_RADIUS_SIZE + ' 0 0 1 ' + 0 + ' ' + (PROCESS_SIZE - PROCESS_RADIUS_SIZE) + ' ' +
             'L ' + 0 + ' ' + PROCESS_RADIUS_SIZE + ' ' +
             'A ' + PROCESS_RADIUS_SIZE + ' ' + PROCESS_RADIUS_SIZE + ' 0 0 1 ' + PROCESS_RADIUS_SIZE + ' ' + 0 + ' ' +
             'z'
-            
+
         jointjs.dia.Element.define('flowchart.Process', {
             attrs: {
                 border: {
                     connection: true,
                     strokeWidth: 4,
-	            stroke: BACKGROUND_COLOR,
-	            fill: 'none',
+                    stroke: BACKGROUND_COLOR,
+                    fill: 'none',
                     strokeLineJoin: 'round',
-		    //filter: { name: 'dropShadow', args: { dx: 1, dy: 1, blur: 1, color: '#223322' } },
+                    //filter: { name: 'dropShadow', args: { dx: 1, dy: 1, blur: 1, color: '#223322' } },
                     d: PROCESS_D,
                     zIndex: 100,
                     magnet: true,
                     cursor: 'pointer',
                 },
-	        body: {
+                body: {
                     connection: true,
                     strokeWidth: 0,
-	            fill: BACKGROUND_COLOR,
+                    fill: BACKGROUND_COLOR,
                     strokeLineJoin: 'round',
                     d: PROCESS_D,
                     zIndex: 5,
-	        },
+                },
                 label: {
                     text: 'name me',
                     textVerticalAnchor: 'middle',
@@ -126,19 +126,19 @@ define([
         });
 
         const DECISION_SIZE = 50;
-        const DECISION_DIAG = Math.sqrt(DECISION_SIZE*DECISION_SIZE*2);
+        const DECISION_DIAG = Math.sqrt(DECISION_SIZE * DECISION_SIZE * 2);
         const RADIUS_SIZE = 3;
-        const DECISION_RADIUS_SIZE = Math.sqrt(RADIUS_SIZE*RADIUS_SIZE/2);
-        const DECISION_RADIUS_OFFSET = Math.sqrt(RADIUS_SIZE*RADIUS_SIZE*2);
-        const DECISION_D = '' + 
-            'M ' + (DECISION_DIAG / 2 - DECISION_RADIUS_SIZE) + ' ' + DECISION_RADIUS_SIZE + ' ' + 
-            'A ' + DECISION_RADIUS_OFFSET + ' ' + DECISION_RADIUS_OFFSET + ' 0 0 1 ' + (DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' + DECISION_RADIUS_SIZE + ' ' + 
-            'L ' + (DECISION_DIAG - DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG / 2 - DECISION_RADIUS_SIZE) + ' ' + 
-            'A ' + DECISION_RADIUS_OFFSET + ' ' + DECISION_RADIUS_OFFSET + ' 0 0 1 ' + (DECISION_DIAG - DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' + 
-            'L ' + (DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG - DECISION_RADIUS_SIZE) + ' ' + 
-            'A ' + DECISION_RADIUS_OFFSET + ' ' + DECISION_RADIUS_OFFSET + ' 0 0 1 ' + (DECISION_DIAG / 2 - DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG - DECISION_RADIUS_SIZE) + ' ' + 
-            'L ' + (DECISION_DIAG / 2 - DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' + 
-            'A ' + DECISION_RADIUS_OFFSET + ' ' + DECISION_RADIUS_OFFSET + ' 0 0 1 ' + (0 + DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG / 2 - DECISION_RADIUS_SIZE) + ' ' + 
+        const DECISION_RADIUS_SIZE = Math.sqrt(RADIUS_SIZE * RADIUS_SIZE / 2);
+        const DECISION_RADIUS_OFFSET = Math.sqrt(RADIUS_SIZE * RADIUS_SIZE * 2);
+        const DECISION_D = '' +
+            'M ' + (DECISION_DIAG / 2 - DECISION_RADIUS_SIZE) + ' ' + DECISION_RADIUS_SIZE + ' ' +
+            'A ' + DECISION_RADIUS_OFFSET + ' ' + DECISION_RADIUS_OFFSET + ' 0 0 1 ' + (DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' + DECISION_RADIUS_SIZE + ' ' +
+            'L ' + (DECISION_DIAG - DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG / 2 - DECISION_RADIUS_SIZE) + ' ' +
+            'A ' + DECISION_RADIUS_OFFSET + ' ' + DECISION_RADIUS_OFFSET + ' 0 0 1 ' + (DECISION_DIAG - DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' +
+            'L ' + (DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG - DECISION_RADIUS_SIZE) + ' ' +
+            'A ' + DECISION_RADIUS_OFFSET + ' ' + DECISION_RADIUS_OFFSET + ' 0 0 1 ' + (DECISION_DIAG / 2 - DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG - DECISION_RADIUS_SIZE) + ' ' +
+            'L ' + (DECISION_DIAG / 2 - DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG / 2 + DECISION_RADIUS_SIZE) + ' ' +
+            'A ' + DECISION_RADIUS_OFFSET + ' ' + DECISION_RADIUS_OFFSET + ' 0 0 1 ' + (0 + DECISION_RADIUS_SIZE) + ' ' + (DECISION_DIAG / 2 - DECISION_RADIUS_SIZE) + ' ' +
             'z';
 
         jointjs.dia.Element.define('flowchart.Decision', {
@@ -146,9 +146,9 @@ define([
                 border: {
                     connection: true,
                     strokeWidth: 4,
-	            stroke: BACKGROUND_COLOR,
-	            fill: 'none',
-		    //filter: { name: 'dropShadow', args: { dx: 1, dy: 1, blur: 1, color: '#223322' } },
+                    stroke: BACKGROUND_COLOR,
+                    fill: 'none',
+                    //filter: { name: 'dropShadow', args: { dx: 1, dy: 1, blur: 1, color: '#223322' } },
                     d: DECISION_D,
                     zIndex: 1,
                     magnet: true,
@@ -157,7 +157,7 @@ define([
                 body: {
                     ref: 'border',
                     connection: true,
-	            fill: BACKGROUND_COLOR,
+                    fill: BACKGROUND_COLOR,
                     d: DECISION_D,
                     zIndex: 5,
                 },
@@ -166,8 +166,8 @@ define([
                     textVerticalAnchor: 'middle',
                     textAnchor: 'middle',
                     ref: 'body',
-                    refX: DECISION_DIAG/2,
-                    refY: DECISION_DIAG/2,
+                    refX: DECISION_DIAG / 2,
+                    refY: DECISION_DIAG / 2,
                     fontSize: 12,
                     fill: 'white',
                     'cursor': 'default',
@@ -193,9 +193,9 @@ define([
             'A ' + STATE_RADIUS + ' ' + STATE_RADIUS + ' 0 0 1 ' + STATE_RADIUS + ' ' + STATE_RADIUS*2 + ' ' +
             'A ' + STATE_RADIUS + ' ' + STATE_RADIUS + ' 0 0 1 ' + 0 + ' ' + STATE_RADIUS + ' ' +
             'z'*/
-            'M ' + STATE_RADIUS + ' ' + 0 + ' ' + 
-            'A ' + STATE_RADIUS + ' ' + STATE_RADIUS + ' 0 0 1 ' + STATE_RADIUS*2 + ' ' + STATE_RADIUS + ' ' + 
-            'A ' + STATE_RADIUS + ' ' + STATE_RADIUS + ' 0 0 1 ' + STATE_RADIUS + ' ' + STATE_RADIUS*2 + ' ' +
+            'M ' + STATE_RADIUS + ' ' + 0 + ' ' +
+            'A ' + STATE_RADIUS + ' ' + STATE_RADIUS + ' 0 0 1 ' + STATE_RADIUS * 2 + ' ' + STATE_RADIUS + ' ' +
+            'A ' + STATE_RADIUS + ' ' + STATE_RADIUS + ' 0 0 1 ' + STATE_RADIUS + ' ' + STATE_RADIUS * 2 + ' ' +
             'A ' + STATE_RADIUS + ' ' + STATE_RADIUS + ' 0 0 1 ' + 0 + ' ' + STATE_RADIUS + ' ' +
             'A ' + STATE_RADIUS + ' ' + STATE_RADIUS + ' 0 0 1 ' + STATE_RADIUS + ' ' + 0 + ' ' +
             'z'
@@ -212,9 +212,9 @@ define([
                     zIndex: 1,*/
                     connection: true,
                     strokeWidth: 4,
-	            stroke: BACKGROUND_COLOR,
-	            fill: 'none',
-		    //filter: { name: 'dropShadow', args: { dx: 1, dy: 1, blur: 1, color: '#223322' } },
+                    stroke: BACKGROUND_COLOR,
+                    fill: 'none',
+                    //filter: { name: 'dropShadow', args: { dx: 1, dy: 1, blur: 1, color: '#223322' } },
                     d: STATE_D,
                     zIndex: 1,
                     magnet: true,
@@ -227,7 +227,7 @@ define([
                     zIndex: 5,*/
                     ref: 'border',
                     connection: true,
-	            fill: BACKGROUND_COLOR,
+                    fill: BACKGROUND_COLOR,
                     d: STATE_D,
                     zIndex: 5,
                 },
@@ -236,9 +236,9 @@ define([
                     textVerticalAnchor: 'middle',
                     textAnchor: 'middle',
                     ref: 'body',
-                    refX: (STATE_RADIUS*2 + 1) / 2,
-                    refY: (STATE_RADIUS*2 + 1) / 2,
-	            fontSize: 12,
+                    refX: (STATE_RADIUS * 2 + 1) / 2,
+                    refY: (STATE_RADIUS * 2 + 1) / 2,
+                    fontSize: 12,
                     fill: 'white',
                     'cursor': 'default',
                 }
@@ -265,7 +265,7 @@ define([
                 },
                 topleft: {
                     ref: 'border',
-                    event: 'topleft:pointerdown', 
+                    event: 'topleft:pointerdown',
                     connection: true,
                     strokeWidth: 1,
                     fill: 'orange',
@@ -278,7 +278,7 @@ define([
                 },
                 left: {
                     ref: 'border',
-                    event: 'topleft:pointerdown', 
+                    event: 'topleft:pointerdown',
                     connection: true,
                     strokeWidth: 1,
                     fill: 'orange',
@@ -291,7 +291,7 @@ define([
                 },
                 bottomleft: {
                     ref: 'border',
-                    event: 'topleft:pointerdown', 
+                    event: 'topleft:pointerdown',
                     connection: true,
                     strokeWidth: 1,
                     fill: 'orange',
@@ -304,7 +304,7 @@ define([
                 },
                 bottom: {
                     ref: 'border',
-                    event: 'topleft:pointerdown', 
+                    event: 'topleft:pointerdown',
                     connection: true,
                     strokeWidth: 1,
                     fill: 'orange',
@@ -317,7 +317,7 @@ define([
                 },
                 bottomright: {
                     ref: 'border',
-                    event: 'topleft:pointerdown', 
+                    event: 'topleft:pointerdown',
                     connection: true,
                     strokeWidth: 1,
                     fill: 'orange',
@@ -330,7 +330,7 @@ define([
                 },
                 right: {
                     ref: 'border',
-                    event: 'topleft:pointerdown', 
+                    event: 'topleft:pointerdown',
                     connection: true,
                     strokeWidth: 1,
                     fill: 'orange',
@@ -343,7 +343,7 @@ define([
                 },
                 topright: {
                     ref: 'border',
-                    event: 'topleft:pointerdown', 
+                    event: 'topleft:pointerdown',
                     connection: true,
                     strokeWidth: 1,
                     fill: 'orange',
@@ -356,7 +356,7 @@ define([
                 },
                 top: {
                     ref: 'border',
-                    event: 'topleft:pointerdown', 
+                    event: 'topleft:pointerdown',
                     connection: true,
                     strokeWidth: 1,
                     fill: 'orange',
@@ -369,7 +369,7 @@ define([
                 },
                 topleft: {
                     ref: 'border',
-                    event: 'topleft:pointerdown', 
+                    event: 'topleft:pointerdown',
                     connection: true,
                     strokeWidth: 1,
                     fill: 'orange',
